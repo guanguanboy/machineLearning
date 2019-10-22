236d4955ea20cdd6c3c0819d315e7f87d9dc9e3c
liguanlin <liguanlin@orbbec.com>
2019-10-22 14:47:58 +0800

    add transferLearning demo

---
+++ TransferLearning/main.py
@@ -0,0 +1,58 @@
+# License: BSD
+# Author: Sasank Chilamkurthy
+
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+plt.ion()   # interactive mode
+
+"""
+我们将使用 torchvision 和 torch.utils.data 包来加载数据。
+
+今天，我们要解决的问题是训练一个模型来对蚂蚁和蜜蜂进行分类。
+我们蚂蚁和蜜蜂分别准备了大约120个训练图像，并且每类还有75个验证图像。
+通常，如果从头开始训练，这是一个非常小的数据集。
+由于我们正在使用迁移学习，我们应该能够合理地进行泛化。
+
+该数据集是imagenet的一个很小的子集。
+"""
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = 'data/hymenoptera_data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
