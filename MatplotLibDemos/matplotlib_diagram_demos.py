d78f79a8f188a8675233b38eb7fbeca82cfba755
liguanlin <liguanlin@orbbec.com>
2019-11-06 09:37:46 +0800

    增加散点图和极坐标图的绘制

--- MatplotLibDemos/matplotlib_diagram_demos.py
+++ MatplotLibDemos/matplotlib_diagram_demos.py
@@ -18,6 +18,7 @@ plt.show()
 """
 
 #直方图
+"""
 np.random.seed(0)
 
 mu,sigma = 100, 20
@@ -27,4 +28,28 @@ a = np.random.normal(mu, sigma, size=100)
 plt.hist(a, 20, normed=1, histtype='stepfilled', facecolor='b', alpha=0.75)
 plt.title('Histogram')
 
+plt.show()
+"""
+
+#散点图
+"""
+fig, ax = plt.subplots()
+
+ax.plot(10 * np.random.randn(100), 10 * np.random.randn(100), 'o')
+ax.set_title('Simple scatter')
+plt.show()
+"""
+N = 10
+theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
+radii = 10 * np.random.rand(N)
+
+width = np.pi / 2 * np.random.rand(N)
+
+ax = plt.subplot(111, projection='polar')
+bars = ax.bar(theta, radii, width=width, bottom = 0.0)
+
+for r, bar in zip(radii, bars):
+	bar.set_facecolor(plt.cm.viridis(r/10.))
+	bar.set_alpha(0.5)
+	
 plt.show()
