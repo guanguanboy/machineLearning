82bb9df2061836a6b9fe699630f6af8031a7d67a
guanguanboy <517445163@qq.com>
2019-11-16 17:51:49 +0800

    调整程序的缩进格式

--- RegularizationDemo/main.py
+++ RegularizationDemo/main.py
@@ -89,8 +89,8 @@ def backward_propagation_with_regularization(X, Y, cache, lamdb):
     return graients
 
 
-def forward_propagation_with_dropout(X, parameters, keep_prob = 0.5)
-	"""
+def forward_propagation_with_dropout(X, parameters, keep_prob = 0.5):
+    """
     Implements the forward propagation: LINEAR -> RELU + DROPOUT -> LINEAR -> RELU + DROPOUT -> LINEAR -> SIGMOID.
     
     Arguments:
@@ -108,47 +108,45 @@ def forward_propagation_with_dropout(X, parameters, keep_prob = 0.5)
     A3 -- last activation value, output of the forward propagation, of shape (1,1)
     cache -- tuple, information stored for computing the backward propagation
     """
-	np.seed(1)
-	
-	#retrieve parameters
-	W1 = parameters["W1"]
-	b1 = parameters["b1"]
-	W2 = parameters["W2"]
-	b2 = parameters["b2"]
-	W3 = parameters["W3"]
-	b3 = parameters["b3"]
-	
-	# LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
-	Z1 = np.dot(W1, X) + b1
-	A1 = relu(Z1)
-	
-	### START CODE HERE ### (approx. 4 lines)         # Steps 1-4 below correspond to the Steps 1-4 described above. 
-    D1 = np.random.rand(A1.shape[0], A1.shape[1])     # Step 1: initialize matrix D1 = np.random.rand(..., ...) 生成一个和A1相同大小的随机数数组
-    D1 = (D1 < keep_prob)                             # Step 2: convert entries of D1 to 0 or 1 (using keep_prob as the threshold)
-    A1 = np.multiply(A1, D1)                          # Step 3: shut down some neurons of A1
-    A1 = A1 / keep_prob                               # Step 4: scale the value of neurons that haven't been shut down
+    np.seed(1)
+    #retrieve parameters
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+    
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+
+    D1 = np.random.rand(A1.shape[0], A1.shape[1])
+    D1 = (D1 < keep_prob)# Step 2: convert entries of D1 to 0 or 1 (using keep_prob as the threshold)
+    A1 = np.multiply(A1, D1)# Step 3: shut down some neurons of A1
+    A1 = A1 / keep_prob# Step 4: scale the value of neurons that haven't been shut down
     ### END CODE HERE ###
-	
-	Z2 = np.dot(W2, A1) + b2
-	A2 = relu(Z2)
-	
-	### START CODE HERE ### (approx. 4 lines)         # Steps 1-4 below correspond to the Steps 1-4 described above. 
+    
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+    
+    ### START CODE HERE ### (approx. 4 lines)         # Steps 1-4 below correspond to the Steps 1-4 described above. 
     D2 = np.random.rand(A2.shape[0], A2.shape[1])     # Step 1: initialize matrix D1 = np.random.rand(..., ...) 生成一个和A1相同大小的随机数数组
     D2 = (D2 < keep_prob)                             # Step 2: convert entries of D1 to 0 or 1 (using keep_prob as the threshold)
     A2 = np.multiply(A2, D2)                          # Step 3: shut down some neurons of A1
     A2 = A2 / keep_prob                               # Step 4: scale the value of neurons that haven't been shut down
     ### END CODE HERE ###
-	
-	Z3 = np.dot(W3, A2) + b3
-	A3 = sigmoid(Z3)
-	
-	cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
-	
-	return A3, cache
-	
+    
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3)
+    
+    cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+    
+    return A3, cache
+    
 
 def backward_propagation_with_dropout(X, Y, cache, keep_prob):
-	"""
+    """
     Implements the backward propagation of our baseline model to which we added dropout.
     
     Arguments:
@@ -160,37 +158,37 @@ def backward_propagation_with_dropout(X, Y, cache, keep_prob):
     Returns:
     gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
     """
-	m = X.shape[1]
-	(Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
-	
-	
-	dZ3 = A3 - Y
-	dW3 = 1./m * np.dot(dZ3, A2.T)
-	db3 = 1./m * np.sum(dZ3, axis = 1, keepdims = True)
-	dA2 = np.dot(W3.T, DZ3)
-	
-	dA2 = np.multiply(dA2, D2) # Step 1: Apply mask D2 to shut down the same neurons as during the forward propagation
-	dA2 = dA2/ keep_prob # Step 2: Scale the value of neurons that haven't been shut down
+    m = X.shape[1]
+    (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    
+    dZ3 = A3 - Y
+    dW3 = 1./m * np.dot(dZ3, A2.T)
+    db3 = 1./m * np.sum(dZ3, axis = 1, keepdims = True)
+    dA2 = np.dot(W3.T, dZ3)
+    
+    dA2 = np.multiply(dA2, D2) # Step 1: Apply mask D2 to shut down the same neurons as during the forward propagation
+    dA2 = dA2/ keep_prob # Step 2: Scale the value of neurons that haven't been shut down
 
-	dZ2 = np.multiply(dA2, np.int64(A2 > 0))
-	dW2 = 1./m * np.dot(dZ2, A1.T)
-	db2 = 1./m * np.sum(dZ2, axis = 1, keepdims = True)
-	dA1 = np.dot(W2.T, DZ2)
-	
-	dA1 = np.multiply(dA2, D2) # Step 1: Apply mask D2 to shut down the same neurons as during the forward propagation
-	dA1 = dA1 / keep_prob # Step 2: Scale the value of neurons that haven't been shut down
-	
-	dZ1 = np.multiply(dA1, np.int64(A1 > 0))
-	dW1 = 1./m * np.dot(dZ1, X.T)
-	db1 = 1./m * np.sum(dZ1, axis = 1, keepdims = True)
-	
-	
-	gradients = {"dZ3":dZ3, "dW3":dW3, "db3":db3, "dA2":dA2, 
-				 "dZ2":dZ2, "dW2":dW2, "db2":db2, "dA1":dA1,
-				 "dZ1":dZ1, "dW1":dW1, "db1":db1}
-				 
-	return gradients
-	
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1./m * np.dot(dZ2, A1.T)
+    db2 = 1./m * np.sum(dZ2, axis = 1, keepdims = True)
+    dA1 = np.dot(W2.T, dZ2)
+    
+    dA1 = np.multiply(dA2, D2) # Step 1: Apply mask D2 to shut down the same neurons as during the forward propagation
+    dA1 = dA1 / keep_prob # Step 2: Scale the value of neurons that haven't been shut down
+    
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1./m * np.dot(dZ1, X.T)
+    db1 = 1./m * np.sum(dZ1, axis = 1, keepdims = True)
+    
+    
+    gradients = {"dZ3":dZ3, "dW3":dW3, "db3":db3, "dA2":dA2, 
+                 "dZ2":dZ2, "dW2":dW2, "db2":db2, "dA1":dA1,
+                 "dZ1":dZ1, "dW1":dW1, "db1":db1}
+                 
+    return gradients
+    
 #Non-regularized model
 def model(X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True, lamdb = 0, keep_prob = 1):
     """
@@ -221,8 +219,8 @@ def model(X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True, 
         #forward propagation:
         if keep_prob == 1:
             a3, cache = forward_propagation(X, parameters)
-        #elif keep_prob < 1:
-            #a3, cache = forward_propagation_with_dropout()
+        elif keep_prob < 1:
+            a3, cache = forward_propagation_with_dropout()
 
         #Cost function
         if lamdb == 0:
@@ -237,8 +235,8 @@ def model(X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True, 
             grads = backward_propagation(X, Y, cache)
         elif lamdb != 0:
             grads = backward_propagation_with_regularization(X, Y, cache, lamdb)
-        #elif keep_prob < 1:
-         #   grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
+        elif keep_prob < 1:
+            grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
 
         # Update parameters.
         parameters = update_parameters(parameters, grads, learning_rate)
