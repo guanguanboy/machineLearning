681eac03822243a730bbea27716867865d81b5e0
liguanlin <liguanlin@orbbec.com>
2019-11-20 17:57:58 +0800

    update scipydemo.py

--- ScipyDemo/scipydemo.py
+++ ScipyDemo/scipydemo.py
@@ -4,12 +4,14 @@ Created on Wed Nov 20 09:31:45 2019
 
 @author: liguanlin
 """
-
+import numpy as np
 from scipy.misc import imread, imsave, imresize
+import matplotlib.pyplot as plt
 
 img = imread('assets/thumbs_up.jpg')
 
 print(img.dtype, img.shape)
+
 
 img_tinted = img * [1, 0.95, 0.9]
 
@@ -17,4 +19,12 @@ img_tinted = imresize(img_tinted, (300, 300))
 
 imsave('assets/thumbs_up_tinted.jpg', img_tinted)
 
-#imshow('assets/thumbs_up_tinted.jpg')
+#show the original image
+plt.subplot(1,2,1)
+plt.imshow(img)
+
+#show the tinted image
+plt.subplot(1,2,2)
+plt.imshow(np.uint8(img_tinted))
+
+plt.show()
